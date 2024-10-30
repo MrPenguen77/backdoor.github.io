7014474ff840e38825aca3877e282131c6ac11fc
MrPenguen77 <teno3162cihanbaba@gmail.com>
2024-10-30 22:36:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,25 +6,22 @@ function showPage(page) {
     } else if (page === 'download') {
         content.innerHTML = '<h1>Download</h1><p>HypnOS Bubbly Blobfish ISO.</p>';
     } else if (page === 'wiki') {
-        content.innerHTML = '<h1>Wiki</h1><p>- HypnOS Sistem Gereksinimleri -</p><p>2 GB RAM (Tavsiye edilen 4 GB RAM)</p><p>20 GB Disk Alanı</p><p>1 GHz İşlemci</p>'; 
-        
-        content.innerHTML += '<p>- Kurulum Rehberi -</p>';
-
-        content.innerHTML += '<p>ISOyu indirin, USB belleğinize yazın, ardından BIOS menüsünden bilgisayarınızı USB belleğinizle başlatın, GRUB menüsünde "Start HypnOS" tuşuna basın, canlı menüde indirme uygulamasını başlatın ve kurulumu bitirin.</p>';
-
-        content.innerHTML += '<p>- HypnOS GitHub Sayfası -</p>';
-
-        content.innerHTML += '<p> https://github.com/MrPenguen77/HypnOS </p>';
-
-        content.innerHTML += '<p>- HypnOS Geliştirici Takımı -</p>';
-
-        content.innerHTML += '<p> MrPenguen - Proje Sorumlusu, Proje Sahibi, Geliştirici, Web Tasarım, Tester, Görsel Tasarım, Sosyal Medya Sorumlusu </p>';
-
-        content.innerHTML += '<p> Kerim - Fikir, Gönüllü Tester </p>';
-        
-        content.innerHTML += '<p>- Nasıl Katkı Yaparım? -</p>';
-
-        content.innerHTML += '<p> Bulduğunuz hataları bize iletip geliştirmemizde yardımcı olup katkı yapabilirsiniz <3 </p>';
+        content.innerHTML = `
+            <h1>Wiki</h1>
+            <h2>HypnOS Sistem Gereksinimleri</h2>
+            <p>2 GB RAM (Tavsiye edilen 4 GB RAM)</p>
+            <p>20 GB Disk Alanı</p>
+            <p>1 GHz İşlemci</p>
+            <h2>Kurulum Rehberi</h2>
+            <p>ISO'yu indirin, USB belleğinize yazın, ardından BIOS menüsünden bilgisayarınızı USB belleğinizle başlatın, GRUB menüsünde "Start HypnOS" tuşuna basın, canlı menüde indirme uygulamasını başlatın ve kurulumu bitirin.</p>
+            <h2>HypnOS GitHub Sayfası</h2>
+            <p><a href="https://github.com/MrPenguen77/HypnOS" target="_blank">https://github.com/MrPenguen77/HypnOS</a></p>
+            <h2>HypnOS Geliştirici Takımı</h2>
+            <p>MrPenguen - Proje Sorumlusu, Proje Sahibi, Geliştirici, Web Tasarım, Tester, Görsel Tasarım, Sosyal Medya Sorumlusu</p>
+            <p>Kerim - Fikir, Gönüllü Tester</p>
+            <h2>Nasıl Katkı Yaparım?</h2>
+            <p>Bulduğunuz hataları bize iletip geliştirmemizde yardımcı olup katkı yapabilirsiniz <3</p>
+        `;
     } else if (page === 'latest') {
         content.innerHTML = `
             <h1>Latest Updates</h1>
