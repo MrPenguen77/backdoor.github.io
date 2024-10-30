04c2bfcf94aabafd72e8b988396b07282c5d911e
MrPenguen77 <teno3162cihanbaba@gmail.com>
2024-10-30 21:46:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function showPage(page) {
     } else if (page === 'download') {
         content.innerHTML = '<h1>Download</h1><p>HypnOS 28.10.24 Xfce Edition.</p>';
     } else if (page === 'wiki') {
-        content.innerHTML = '<h1>Wiki</h1><p>HypnOS System Requirements:</p><p>2 GB RAM (Recommended 4 GB RAM)</p><p>20 GB Disk Space</p><p>1 GHz Processor</p>'; 
+        content.innerHTML = '<h1>Wiki</h1><p>- HypnOS System Requirements:</p><p>2 GB RAM (Recommended 4 GB RAM)</p><p>20 GB Disk Space</p><p>1 GHz Processor</p>'; 
+        
+        content.innerHTML += '<p>- Install Guide</p>';
+
+        content.innerHTML += '<p>Download the ISO, burn it to your USB memory, then start your computer with your USB memory from the BIOS menu, press "Start HypnOS" in the GRUB menu, start the download application in the live menu and finish the installation.</p>';
     } else if (page === 'latest') {
         content.innerHTML = `
             <h1>Latest Updates</h1>
