7fd8f714fe6c1a2377d5c60b799bee470c66d851
MrPenguen77 <teno3162cihanbaba@gmail.com>
2024-10-30 19:31:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function showPage(page) {
     const content = document.getElementById('content');
 
     if (page === 'home') {
-        content.innerHTML = '<h1>Home</h1><p>HypnOS is an Ubuntu-based GNU/Linux distribution developed for cybersecurity.</p>';
+        content.innerHTML = '<h1>Home</h1><p>HypnOS is an Ubuntu based GNU/Linux distribution developed for cybersecurity.</p>';
     } else if (page === 'download') {
         content.innerHTML = '<h1>Download</h1><p>HypnOS 28.10.24 Xfce Edition.</p>';
     } else if (page === 'wiki') {
