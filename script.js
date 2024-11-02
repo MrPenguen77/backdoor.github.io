01592537e0444d3db2bea2cc3695a2bf06962f44
MrPenguen77 <teno3162cihanbaba@gmail.com>
2024-11-02 13:51:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
         if (page === 'home') {
             content.innerHTML = '<h1>Home</h1><p>- HackUbuntu -</p>';
             content.innerHTML += '<p>HackUbuntu hafif, güçlü ve hızlı olup Ubuntu tabanlıdır.</p>';
+
+            content.innerHTML += '<img src="dev.png" alt="Developer Team Logo" width="200">';
+            content.innerHTML += '<p>- HackUbuntu Linux Geliştirici Takımı -</p>';
+            content.innerHTML += '<p>MrPenguen - Görev: Takım Lideri, Geliştirici</p>';
+            content.innerHTML += '<p>LordBalbazar - Görev: Tester</p>';
+            content.innerHTML += '<p>Tamelektro - Görev: Tester</p>';
+            content.innerHTML += '<p>HamTux - Görev: Geliştirici</p>
         } else if (page === 'download') {
             content.innerHTML = '<h1>İndir</h1><p>HackUbuntu Bubbly Blobfish ISO.</p>';
         } else if (page === 'wiki') {
@@ -13,12 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
             content.innerHTML += '<p>ISO dosyasını indirin, USB belleğinize yazın, ardından BIOS menüsünden bilgisayarınızı USB belleğinizle başlatın, GRUB menüsünde "Start HackUbuntu" tuşuna basın, canlı menüde indirme uygulamasını başlatın ve kurulumu bitirin.</p>';
             content.innerHTML += '<p>- HackUbuntu GitHub Sayfası -</p>';
             content.innerHTML += '<p><a href="https://github.com/MrPenguen77/hackubuntu" target="_blank">GitHub HackUbuntu</a></p>';
-            content.innerHTML += '<img src="dev.png" alt="Developer Team Logo" width="200">';
-            content.innerHTML += '<p>- HackUbuntu Linux Geliştirici Takımı -</p>';
-            content.innerHTML += '<p>MrPenguen - Görev: Takım Lideri, Geliştirici</p>';
-            content.innerHTML += '<p>LordBalbazar - Görev: Tester</p>';
-            content.innerHTML += '<p>Tamelektro - Görev: Tester</p>';
-            content.innerHTML += '<p>HamTux - Görev: Geliştirici</p>
         } else if (page === 'latest') {
             content.innerHTML = 
                 '<h1>Son Güncellemeler</h1>' +
